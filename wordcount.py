84e0ccf0e760956222bda014de2e2796aa3c4284
Carlos A Jaime <cajaime@miners.utep.edu>
2020-08-28 20:49:37 -0600

    Beta Version 0.1

---
+++ wordcount.py
@@ -0,0 +1,45 @@
+#! /usc/bin/env python3
+import sys
+import os
+
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+
+if len(sys.argv) is not 3:
+    print("Correct Usage $python3 wordcount.py <your text file> <your output file>")
+    exit()
+
+ 
+input_text = open(inputfile,'r')
+word_Dict = dict()
+outtext = dict()
+
+if not os.path.exists(inputfile):
+    print("No Input file of that name exists")
+    exit()
+
+if not os.path.exists(outputfile):
+    print("No Outputfile of that name exists")
+    exit()
+for line in input_text:
+    line = line.strip()
+    line = line.lower()
+    words = line.split(" ")
+
+    for word in words:
+        if word in word_Dict:
+            word_Dict[word] = word_Dict[word] + 1
+        else:
+            word_Dict[word] = 1
+            
+## Open and write onto file
+out = open(outputfile, 'w')
+for key, value in sorted(word_Dict.items(), key = lambda x: x[1], reverse=True):
+    out.write(str(key) + ' ' + str(value) + '\n')
+
+    
+##Close Files
+input_text.close()
+out.close()
+    
+
